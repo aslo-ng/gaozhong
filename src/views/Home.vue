c5553d59c677ad285b730ace6893c273830543f6
鲁显龙 <2032824554@qq.com>
2021-11-17 10:49:23 +0800

    添加图片

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <div class="img">
+      <img src="@/assets/images2/国际航空服务与管理_02.jpg" alt="">
+    </div>
     <div class="title_warp">
       <p>
         <router-link to="/" tag="span">当前位置：首页 ></router-link>
@@ -18,6 +21,18 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.home{
+  width: 3.75rem;
+  margin: 0 auto;
+}
+.img{
+  width: 100%;
+  height: 1.49rem;
+  img{
+    width: 100%;
+    height:100%;
+  }
+}
 .title_warp{width:100%;padding:0 .2rem;margin: .15rem 0;box-sizing:border-box;
 p{width: 100%;text-align:left;
 span {font-size: .14rem;font-weight: normal;font-stretch: normal;line-height: .07rem;letter-spacing: 0px;color: #888888;}
